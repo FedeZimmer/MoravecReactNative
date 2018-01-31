ffac55e457ee525621cbdcecea7c6cd7733113e5
Luciano Leveroni <lleveroni@eryx.co>
2018-01-30 22:08:17 -0300

    Refactor
    - Add redux-thunk

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import {AppRegistry} from "react-native";
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
 import {Provider} from "react-redux";
 import {combineReducers} from 'redux';
+import thunkMiddleware from 'redux-thunk'
 import {gameReducer} from "./src/reducers/game_reducer";
 import {Navigator} from "./src/navigator";
 
 
-const reducer = combineReducers({
+const rootReducer = combineReducers({
     game: gameReducer,
 });
 
-const store = createStore(reducer);
+const store = createStore(rootReducer, undefined, applyMiddleware(thunkMiddleware));
 
 const Moravec = () => (
     <Provider store={store}>
