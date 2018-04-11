77f10426ae9fd7c517afefe97b3395ed88bf2ac2
Maximiliano Suppes <msuppes@eryx.co>
2018-04-11 19:45:22 -0300

    Cambiar el orden de los números en la calculadora:
    - Changed order of numbers; The are ordered from lowest to highest

--- src/components/game/calculator/keyboard/CalculatorKeyboard.js
+++ src/components/game/calculator/keyboard/CalculatorKeyboard.js
@@ -17,9 +17,9 @@ export default class CalculatorKeyboard extends React.Component {
             <View style={INPUT_STYLES.inputContainer}>
                 <View style={INPUT_STYLES.input}>
                     <View style={INPUT_STYLES.row1}>
-                        <NumberKey value={7} onTypeInput={this.props.onTypeInput}/>
-                        <NumberKey value={8} onTypeInput={this.props.onTypeInput}/>
-                        <NumberKey value={9} onTypeInput={this.props.onTypeInput}/>
+                        <NumberKey value={1} onTypeInput={this.props.onTypeInput}/>
+                        <NumberKey value={2} onTypeInput={this.props.onTypeInput}/>
+                        <NumberKey value={3} onTypeInput={this.props.onTypeInput}/>
                     </View>
                     <View style={INPUT_STYLES.row2}>
                         <NumberKey value={4} onTypeInput={this.props.onTypeInput}/>
@@ -27,9 +27,9 @@ export default class CalculatorKeyboard extends React.Component {
                         <NumberKey value={6} onTypeInput={this.props.onTypeInput}/>
                     </View>
                     <View style={INPUT_STYLES.row3}>
-                        <NumberKey value={1} onTypeInput={this.props.onTypeInput}/>
-                        <NumberKey value={2} onTypeInput={this.props.onTypeInput}/>
-                        <NumberKey value={3} onTypeInput={this.props.onTypeInput}/>
+                        <NumberKey value={7} onTypeInput={this.props.onTypeInput}/>
+                        <NumberKey value={8} onTypeInput={this.props.onTypeInput}/>
+                        <NumberKey value={9} onTypeInput={this.props.onTypeInput}/>
                     </View>
                     <View style={INPUT_STYLES.row4}>
                         <EraseKey onEraseInput={this.props.onEraseInput}/>
