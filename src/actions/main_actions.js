041b31c470563fd3ef13785f6270fb24c9a8fbe4
Lucas Rodriguez <lrodriguez@eryx.co>
2018-07-16 19:16:58 -0300

    Ajustes:
    - Se saco el spinner de los tutoriales

--- src/actions/main_actions.js
+++ src/actions/main_actions.js
@@ -6,7 +6,7 @@ export function verifyIfPersonalInfoIsSavedOnDevice(callback) {
         let personalInfoIsCompleted = false;
         AsyncStorage.getItem('@moravec:personalInfo').then((personalInfoJSON) => {
             if (personalInfoJSON !== null) {
-                personalInfoIsCompleted = false;
+                personalInfoIsCompleted = true;
                 const savedPersonalInfo = JSON.parse(personalInfoJSON);
 
                 if (!savedPersonalInfo.sentToBackend) {
--- src/components/tutorials/Tutorial.js
+++ src/components/tutorials/Tutorial.js
@@ -1,35 +1,11 @@
 import React from "react";
-import Video from "react-native-video";
-import VideoPlayer from 'react-native-video-controls';
+import VideoPlayer from "react-native-video-controls";
 import {ScrollView, Text, View, Image} from "react-native";
-import {Spinner} from "native-base";
-import {spinnerColor, TUTORIAL_STYLES} from "../../styles/tutorials/styles";
+import {TUTORIAL_STYLES} from "../../styles/tutorials/styles";
 
 export class Tutorial extends React.Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            loadingVideo: true,
-            height: 0,
-        };
-
-        this.videoLoaded = this.videoLoaded.bind(this);
-        this.showSpinner = this.showSpinner.bind(this);
-    }
-
-    videoLoaded() {
-        this.setState({height: 200, loadingVideo: false});
-    }
-
-    showSpinner() {
-        if (this.state.loadingVideo) {
-            return (
-                <Spinner color={spinnerColor}/>
-            )
-        } else {
-            return null;
-        }
     }
 
     render() {
@@ -40,10 +16,10 @@ export class Tutorial extends React.Component {
                 </View>
                 <View style={TUTORIAL_STYLES.division}>
                 </View>
-                {this.showSpinner()}
-                <VideoPlayer source={{uri: this.props.videoUrl}} resizeMode="stretch" paused={true}
-                             onLoad={this.videoLoaded} toggleResizeModeOnFullscreen={false} disableVolume={true}
-                             disableFullscreen={true} disableBack={true}/>
+                <VideoPlayer source={{uri: this.props.videoUrl}} onPress={() => null}
+                             resizeMode="stretch" paused={true} onLoad={this.videoLoaded}
+                             toggleResizeModeOnFullscreen={false} disableVolume={true} disableFullscreen={true}
+                             disableBack={true}/>
                 {this.props.showExamples()}
             </ScrollView>
         );
