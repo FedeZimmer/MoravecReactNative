94aed25a8c341da8bdbf8ee58684a251dbc209d6
Maximiliano Suppes <msuppes@eryx.co>
2018-09-21 11:32:17 -0300

    Cuentas que desaparecen con el tiempo:
    - Todos los niveles tiene una probabilidad de que una operación desaparezca de la pantalla. A partir de esta probabilidad, si una operación debe desaparecer, lo hace al cabo de 5 segundos. Siempre se muestran el "=" y el resultado

--- src/components/game/calculator/OperationDisplay.js
+++ src/components/game/calculator/OperationDisplay.js
@@ -1,13 +1,43 @@
 import React from 'react'
-import {Text, View} from "react-native";
-
+import {Text, View, Animated} from "react-native";
 import {OPERATION_STYLES} from "../../../styles/game/calculator/styles";
 import {makeItTestable} from "../../../utils/testable_hoc";
 
-export let OperationDisplay = class extends React.Component {
+export let OperationDisplay = class extends React.Component{
     constructor(props) {
         super(props);
+
+        this.state = {
+            opacityValue: new Animated.Value(1)
+        };
+
         this.operationFontSize = this.operationFontSize.bind(this);
+        this.fadeout = this.fadeout.bind(this);
+    }
+
+    componentDidMount() {
+        if (this.props.operation.shouldBeHidden) {
+            this.fadeout();
+        }
+    }
+
+    componentDidUpdate(prevProps) {
+         const newOperationShown = prevProps.operation.operation !== this.props.operation.operation;
+         if (newOperationShown) {
+             this.setState({opacityValue: new Animated.Value(1)}, () => {
+                 if (this.props.operation.shouldBeHidden) {
+                     this.fadeout();
+                 }
+             });
+         }
+    }
+
+    operationResult() {
+        if (this.props.input) {
+            return this.props.input
+        } else {
+            return '?';
+        }
     }
 
     operationFontSize() {
@@ -22,20 +52,32 @@ export let OperationDisplay = class extends React.Component {
         return operationFontSize;
     }
 
-    operationResult() {
-        if (this.props.input) {
-            return this.props.input
-        } else {
-            return '?';
-        }
+    fadeout() {
+        const fadeoutAnimation = Animated.timing(
+            this.state.opacityValue,
+            {
+                delay: 4000,
+                toValue: 0,
+                duration: 1000,
+            }
+        );
+        fadeoutAnimation.start();
     }
 
     render() {
+        const textStyle = [OPERATION_STYLES.operationText, {fontSize: this.operationFontSize()}];
         return (
             <View style={OPERATION_STYLES.operationContainer}>
                 <View style={OPERATION_STYLES.operation}>
-                    <Text style={[OPERATION_STYLES.operationText, {fontSize: this.operationFontSize()}]}>
-                        {this.props.operation.operation}{' = '}{this.operationResult()}
+                    <Animated.View style={{opacity: this.state.opacityValue}}>
+                        <Text style={textStyle}>
+                            {this.props.operation.operation}
+                        </Text>
+                    </Animated.View>
+                    <Text>
+                        <Text style={textStyle}>
+                            {' = '}{this.operationResult()}
+                        </Text>
                     </Text>
                 </View>
             </View>
