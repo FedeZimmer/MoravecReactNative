78dc1c0ef4f46557360641eba872bf6b55fe5851
Lucas Rodriguez <lrodriguez@eryx.co>
2018-12-12 17:37:53 -0300

    - Se elimina 'console.log' en codigo nativo

--- ios/Moravec/AppDelegate.m
+++ ios/Moravec/AppDelegate.m
@@ -21,16 +21,6 @@
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
-  for (NSString* family in [UIFont familyNames])
-  {
-      NSLog(@"%@", family);
-
-      for (NSString* name in [UIFont fontNamesForFamilyName: family])
-      {
-          NSLog(@"  %@", name);
-      }
-  }
-
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"Moravec"
                                                initialProperties:nil
